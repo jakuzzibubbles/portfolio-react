bb86eee76d57c3884ae7bde8450e2c7bc0be8b78
jakuzzibubbles <jakuzzi263@gmail.com>
2024-10-27 02:09:17 +0200

    installed three and removed readme

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -4,28 +4,34 @@ import Skills from './components/Skills';
 import WorkExperience from './components/WorkExperience';
 import AboutMe from './components/AboutMe';
 import Contact from './components/Contact';
-import BG_MAGENTA from './assets/bg-magenta.jpg';
 import Footer from './components/Footer';
+import "./index.css";
+import StarFieldBackground from "./StarFieldBackground";
 
 const App = () => {
   return (
-    <div className='bg-slate-950'>
+    <div className='bg-slate-950' style={{ position: 'relative', overflow: 'hidden' }}>
+      
+    
       <div
         style={{
-          backgroundImage: `url(${BG_MAGENTA})`,
+          position: 'relative',
+          zIndex: 1,
           backgroundPosition: `50% 50%`,
         }}
       >
         <NavBar />
         <Hero />
+        </div>
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <Skills />
+        <WorkExperience />
+        <AboutMe />
+        <Contact />
+        <Footer />
       </div>
-      <Skills />
-      <WorkExperience />
-      <AboutMe />
-      <Contact />
-      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
