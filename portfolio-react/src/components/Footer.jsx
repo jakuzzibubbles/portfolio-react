793162ac59553bdc33d0e2eefa7a2374fe587ddc
Linh Nguyen <jakuzzi263@gmail.com>
2024-11-08 23:19:03 +0100

    Update Footer.jsx

--- portfolio-react/src/components/Footer.jsx
+++ portfolio-react/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-sky-900 text-xs text-center p-5'>
-      Made with 🩷 Happy Coding!
+      Made with 🩷!
     </div>
   );
 };
